08cd071040dae00b37afc129a95f1605bfd84f8b
habibiiberahim <habebc@gmail.com>
2025-11-19 11:43:52 +0800

    feat: implement observer pattern

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"golang-design-pattern/behavioral/observer"
 	"golang-design-pattern/creational/abstract_factory"
 	"golang-design-pattern/creational/factory_method"
 )
@@ -8,4 +9,5 @@ import (
 func main() {
 	factory_method.RunFactoryMethod()
 	abstract_factory.RunAbstractFactory()
+	observer.RunObserver()
 }
